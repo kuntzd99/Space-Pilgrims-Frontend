21c00ed9958be7c82d7b92248e926af06f52687a
Chinwendu <chinwenduukoha@gmail.com>
2022-04-16 08:02:04 -0400

    register page initiated, minimum styling done on nav component

---
+++ client/src/pages/Register.js
@@ -0,0 +1,98 @@
+import { useState } from 'react'
+import { RegisterPilgrim } from '../services/Auth'
+import { useNavigate } from 'react-router-dom'
+
+const Register = () => {
+  let navigate = useNavigate()
+
+  const [formValues, setFormValues] = useState({
+    username: '',
+    email: '',
+    password: '',
+    confirmPassword: ''
+  })
+
+  const handleChange = (e) => {
+    setFormValues({ ...formValues, [e.target.name]: e.target.value })
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    await RegisterPilgrim({
+      name: formValues.username,
+      email: formValues.email,
+      password: formValues.password
+    })
+    setFormValues({
+      username: '',
+      email: '',
+      password: '',
+      confirmPassword: ''
+    })
+    navigate('/login')
+  }
+
+  return (
+    <div className="col">
+      <div className="card-overlay centered">
+        <form className="forms" onSubmit={handleSubmit}>
+          <div className="input-wrapper">
+            <label htmlFor="name">Username</label>
+            <input
+              onChange={handleChange}
+              name="username"
+              type="text"
+              placeholder="CoolPigrim08"
+              value={formValues.username}
+              required
+            />
+          </div>
+          <div className="input-wrapper">
+            <label htmlFor="email">Email</label>
+            <input
+              onChange={handleChange}
+              name="email"
+              type="email"
+              placeholder="pilgrim@pilgrim.com"
+              value={formValues.email}
+              required
+            />
+          </div>
+
+          <div className="input-wrapper">
+            <label htmlFor="password">Password</label>
+            <input
+              onChange={handleChange}
+              type="password"
+              name="password"
+              value={formValues.password}
+              required
+            />
+          </div>
+          <div className="input-wrapper">
+            <label htmlFor="confirmPassword">Confirm Password</label>
+            <input
+              onChange={handleChange}
+              type="password"
+              name="confirmPassword"
+              value={formValues.confirmPassword}
+              required
+            />
+          </div>
+          <button
+            className="registerbtn"
+            disabled={
+              !formValues.email ||
+              (!formValues.password &&
+                formValues.confirmPassword === formValues.password)
+            }
+          >
+            Sign In
+          </button>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default Register
