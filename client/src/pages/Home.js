f24a9ed452a74546f34f848c5e86bba4a8575e9d
Mark Kennedy <kennedymark@hotmail.com>
2022-04-16 10:47:27 -0400

    Added red planet

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,7 +2,15 @@ const Home = () => {
   return (
     <div className="solar-system">
       <div className="blue" onClick={() => console.log('click blue')}></div>
-      <div className="red" onClick={() => console.log('click white')}></div>
+      <div
+        className="blue-planet"
+        onClick={() => console.log('click blue')}
+      ></div>
+      <div className="red" onClick={() => console.log('click red')}></div>
+      <div
+        className="red-planet"
+        onClick={() => console.log('click red')}
+      ></div>
     </div>
   )
 }
