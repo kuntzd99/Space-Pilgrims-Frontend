84a6df139325b4ac093ee00a70daffb844d29ac8
Chinwendu <chinwenduukoha@gmail.com>
2022-04-18 14:29:29 -0400

    create pilgrim page moved to register page

--- client/src/components/CreatePilgrim.jsx
+++ client/src/components/CreatePilgrim.jsx
@@ -1,8 +1,19 @@
-import axios from "axios"
-
 const CreatePilgrim = () => {
+
+  const [formValues, setFormValues] = useState({name: '', image: '', bio: ''})
+
+  const handleChange = (e) => {
+    setFormValues({ ...formValues, [e.target.name]: e.target.value })
+  }
   return (
-    <div></div>
+    <div>
+      <label>Full Name</label>
+      <input onChange={handleChange} type="text" name="name" value={formValues.name} required />
+      <label>Upload Image:</label>
+      <input onChange={handleChange} type="file" className="pilgrimImage" name="image" required />
+      <label>Add Bio</label>
+      <textarea onChange={handleChange} type="text" name="bio" required />
+    </div>
   )
 }
 
