6e152a97756712abde3a076d44c38e856dd64b8c
Mark Kennedy <kennedymark@hotmail.com>
2022-04-18 20:17:17 -0400

    create the start of the carousel.

--- client/src/pages/PlanetPage.js
+++ client/src/pages/PlanetPage.js
@@ -19,11 +19,18 @@ const PlanetPage = (props) => {
   }, [])
 
   return (
-    <div>
+    <div className="planet-page">
       <h1>{props.planet.name}</h1>
       <img src={props.planet.image} alt={props.planet.name} />
       <p>{props.planet.description}</p>
       <h3>Population: {props.planet.population}</h3>
+      <div className="carousel">
+        <div className="child bg-1"></div>
+        <div className="child bg-2"></div>
+        <div className="child bg-3"></div>
+        <div className="child bg-4"></div>
+        <div className="child bg-5"></div>
+      </div>
       <h1>Communities:</h1>
       <Community
         communities={props.communities}
